5a3b857364fb3ac79e5235554e3481befeb3f981
Harsha <harshavardhanindla11@gmail.com>
2023-10-27 17:59:31 +0530

    changes

--- react-01/src/Components/MenuCard.js
+++ react-01/src/Components/MenuCard.js
@@ -3,50 +3,50 @@ import Shimmer from "./Shimmer";
 import { MENU_API } from "../utils/URLs";
 import { useParams } from "react-router-dom";
 const MenuCard = () => {
-    const[filteredResInfo1, setfilteredResInfo1] = useState(null);
-    const {
-      resId
-    } = useParams();
+  const [filteredResInfo1, setfilteredResInfo1] = useState(null);
+  const { resId } = useParams();
 
-    useEffect(() => {
-        fetchData();
+  useEffect(() => {
+    fetchData();
 
-        return ()=>{
-          setfilteredResInfo1(null)
-        }
-    },[])
-    const fetchData = async () => {
-      console.log("menu render")
-       const data = await fetch(MENU_API + resId)
-        const datajson = await data.json();
-        setfilteredResInfo1(datajson.data)
-        
-    }
+    return () => {
+      setfilteredResInfo1(null);
+    };
+  }, []);
+  const fetchData = async () => {
+    console.log("menu render");
+    const data = await fetch(MENU_API + resId);
+    const datajson = await data.json();
+    setfilteredResInfo1(datajson.data);
+  };
 
-    if (filteredResInfo1 === null) return <Shimmer />;
-    
+  if (filteredResInfo1 === null) return <Shimmer />;
 
-      return (
+  return (
     <div>
       <h1>{filteredResInfo1?.cards[0].card.card.info.name}</h1>
       <p>
-        {filteredResInfo1?.cards[0].card.card.info.cuisines.join(", ")} - {filteredResInfo1?.cards[0].card.card.info.costForTwoMessage}
+        {filteredResInfo1?.cards[0].card.card.info.cuisines.join(", ")} -{" "}
+        {filteredResInfo1?.cards[0].card.card.info.costForTwoMessage}
       </p>
       <h2>Menu</h2>
       <ul>
-        {console.log(filteredResInfo1.cards[2].groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card)}
-         {
-          
-          filteredResInfo1?.cards[2].groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card?.carousel?.map(res => {
-                return (
-                    <li>{res.dish.info.name}: {"Rs "+  res.dish.info.price/100 }</li>
-                )
-            })
-        }
+        {console.log(
+          filteredResInfo1.cards[2].groupedCard?.cardGroupMap?.REGULAR?.cards[1]
+            ?.card?.card
+        )}
+        {filteredResInfo1?.cards[2].groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card?.carousel?.map(
+          (res) => {
+            return (
+              <li>
+                {res.dish.info.name}: {"Rs " + res.dish.info.price / 100}
+              </li>
+            );
+          }
+        )}
       </ul>
     </div>
   );
-       
 };
 
-export default MenuCard
+export default MenuCard;
